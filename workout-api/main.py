11185aed8551648b1e4a6fa06096f41de4257862
Odair H <odair.sp@hotmail.com>
2024-11-05 15:15:32 -0300

    first commit

---
+++ workout-api/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn
+
+
+app = FastAPI(title="WorkoutAPI")
+
+if __name__ == 'main':
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, log_level='info', reload=True)
+    
+@app.get("/")
+def read_root(): 
+    return {"Hello": "World"}
+
+
+
+ 
